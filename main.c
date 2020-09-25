d085457081afedbea74a6b78011e2636353de7cc
Waylon Cude <waylon@muse.dev>
2020-09-25 00:16:44 -0700

    Break more things
    Infer doesn't see the null pointer dereference on calls to library
    functions

--- main.c
+++ main.c
@@ -5,6 +5,10 @@ static char text[] = "Hello Muse!";
 int main() {
     char * printme = text;
     printme = NULL;
+    printme[0] = 'X';
+    printme[1] = 'X';
+    printme[2] = 'X';
+    printme[3] = '\0';
     printf("%s\n",printme);
     return 0;
 }
